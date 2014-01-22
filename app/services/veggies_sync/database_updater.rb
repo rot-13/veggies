da8a2ea42fd0bc9a00a5892ae1993b7d098ed680
Shay Davidson <shay.h.davidson@gmail.com>
2014-01-22 17:51:24 +0200

    Added find or create veggie

--- app/services/veggies_sync/database_updater.rb
+++ app/services/veggies_sync/database_updater.rb
@@ -3,9 +3,15 @@ module VeggiesSync
   class DatabaseUpdater
 
     def update_from(fetched_veggies)
-      fetched_veggies.each do |veggie|
-
+      fetched_veggies.each do |fetched_veggie|
+        veggie = find_or_create_veggie(fetched_veggie)
       end
+    end
+
+    private
+
+    def find_or_create_veggie(fetched_veggie)
+      Veggie.where(:heb_name => fetched_veggie.heb_name).first_or_create
     end
 
   end
