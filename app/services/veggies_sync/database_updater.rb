944e4471b1cd6ab3f446edd97705a0ace7647333
Shay Davidson <shay.h.davidson@gmail.com>
2014-01-22 21:06:34 +0200

    Updating prices works

--- app/services/veggies_sync/database_updater.rb
+++ app/services/veggies_sync/database_updater.rb
@@ -2,9 +2,16 @@ module VeggiesSync
 
   class DatabaseUpdater
 
-    def update_from(fetched_veggies)
-      fetched_veggies.each do |fetched_veggie|
-        veggie = find_or_create_veggie(fetched_veggie)
+    def initialize(fetched_veggies)
+      @fetched_veggies = fetched_veggies
+    end
+
+    def update
+      ActiveRecord::Base.transaction do
+        @fetched_veggies.each do |fetched_veggie|
+          persisted_veggie = find_or_create_veggie(fetched_veggie)
+          add_price_to_veggie(persisted_veggie, fetched_veggie)
+        end
       end
     end
 
@@ -12,6 +19,13 @@ module VeggiesSync
 
     def find_or_create_veggie(fetched_veggie)
       Veggie.where(:heb_name => fetched_veggie.heb_name).first_or_create
+    end
+
+    def add_price_to_veggie(persisted_veggie, fetched_veggie)
+      last_price = persisted_veggie.prices.last
+      if last_price.nil? || (last_price.created_at.to_date != fetched_veggie.date)
+        persisted_veggie.prices.create(fetched_veggie.prices)
+      end
     end
 
   end
--- app/services/veggies_sync/fetched_veggie.rb
+++ app/services/veggies_sync/fetched_veggie.rb
@@ -2,10 +2,16 @@ module VeggiesSync
 
   class FetchedVeggie < Hashie::Dash
 
+    attr_reader :prices
+
     property :date
     property :heb_name
     property :primary_price
     property :secondary_price
+
+    def prices
+      slice(:primary_price, :secondary_price)
+    end
 
   end
 
